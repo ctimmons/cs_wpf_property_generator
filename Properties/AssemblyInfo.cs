aa01ea2dd74cc0877006e7fb5e073a0492d8a352
ctimmons <chris@crtimmonsinc.com>
2019-08-12 18:15:24 -0500

    Updated AssemblyInfo.cs.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IChangeTrackingCodeGen")]
+[assembly: AssemblyTitle("cs_wpf_property_generator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IChangeTrackingCodeGen")]
+[assembly: AssemblyProduct("cs_wpf_property_generator")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
